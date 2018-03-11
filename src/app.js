38238a31502d973b9ddaa377c7a7e1f7777f66cf
R-Newalsing <R-Newalsing@users.noreply.github.com>
2018-03-11 22:31:12 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 require('./bootstrap');
 
 const config = {
-    apiKey: "AIzaSyBBDOcqvSiw8JUCD50ZQ-qtTY-Q5HudkNE",
-    authDomain: "rate-your-colleague.firebaseapp.com",
-    databaseURL: "https://rate-your-colleague.firebaseio.com",
-    storageBucket: "rate-your-colleague.appspot.com",
-    messagingSenderId: "878714293648"
+
 };
 
 firebase.initializeApp(config);
